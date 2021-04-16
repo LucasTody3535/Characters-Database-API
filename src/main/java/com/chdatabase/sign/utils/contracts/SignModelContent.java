eab3d1fcf83a7389796a4995bf42519a021e9dbc
LucasTody3535 <lucas.web3535@gmail.com>
2021-04-16 20:24:59 -0300

    Estabelecido a regra de campos para o SignModel

---
+++ src/main/java/com/chdatabase/sign/utils/contracts/SignModelContent.java
@@ -0,0 +1,12 @@
+package com.chdatabase.sign.utils.contracts;
+
+import com.chdatabase.game.models.ClassGenre;
+
+public interface SignModelContent {
+
+	final String name = "";
+	final String nickName = "";
+	final String password = "";
+	final ClassGenre classGenre = null;
+	
+}
